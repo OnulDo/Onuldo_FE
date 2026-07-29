ece929521bef391d8e2196c2ff5d16e59ffebad0
hayeoniill <lhy5441@naver.com>
2026-07-29 19:45:41 +0900

    feat: 디자인 시스템 BlackBrown70 추가

--- app/src/main/java/com/example/onuldo_fe/ui/theme/Color.kt
+++ app/src/main/java/com/example/onuldo_fe/ui/theme/Color.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.graphics.Color
 val Black = Color(0xFF101010)
 val White = Color(0xFFFFFFFF)
 val BlackBrown = Color(0xFF1B130C)
+val BlackBrown70 = BlackBrown.copy(alpha = 0.7f)
 
 //Background
 val SourCream = Color(0xFFFFFDF7)
