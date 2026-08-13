f2ed4c2eb3eac3c05cb0d442b275a15ab385ba92
goomin <rkdalsrna@gachon.ac.kr>
2026-08-13 13:05:09 +0900

    fix: 공용 버튼 textStyle 중복 선언 제거

--- app/src/main/java/com/example/onuldo_fe/ui/component/OnulDoButton.kt
+++ app/src/main/java/com/example/onuldo_fe/ui/component/OnulDoButton.kt
@@ -39,7 +39,6 @@ fun OnulDoButton(
     disabledContainerColor: Color = BlackBrown.copy(alpha = 0.1f),
     disabledContentColor: Color = BlackBrown.copy(alpha = 0.2f),
     pressedContainerColor: Color = DarkBrown,
-    textStyle: TextStyle = OnulDoTypography.body2Bold,
 ) {
     val interactionSource = remember { MutableInteractionSource() }
     val isPressed by interactionSource.collectIsPressedAsState()
